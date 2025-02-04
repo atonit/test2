1d86bc0da42a147208e47f15828c3faf6415ffbd
atonit <sonamagat@gmail.com>
2025-02-04 21:09:29 +0100

    Version 2.0: Agregado boton con evento de click

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Página cargada');
 });
+
+document.getElementById('clickMe').addEventListener('click', () => {
+    alert('¡Botón clicado!');
+});
